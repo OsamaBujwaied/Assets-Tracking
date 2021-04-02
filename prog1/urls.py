d4d2e83beb1ee450ba323748f3a40d61db89940e
Osama Bujwaied <78872869+OsamaBujwaied@users.noreply.github.com>
2021-04-02 21:27:49 +0300

    Update urls.py

--- prog1/urls.py
+++ prog1/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('market/', include('app1.urls')),
     path('assets/', include('assetstracking.urls')),
 ]
