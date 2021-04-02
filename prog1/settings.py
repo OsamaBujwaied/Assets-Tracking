ba431a52b9ed22e69c8a3d77f9904c29f0d8ba6d
Osama Bujwaied <78872869+OsamaBujwaied@users.noreply.github.com>
2021-04-02 19:05:00 +0300

    Update settings.py

--- prog1/settings.py
+++ prog1/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddlewate',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
