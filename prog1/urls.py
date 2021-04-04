bc2a1a116437af14c46f4489bf664941ab4bf145
Osama Bujwaied <78872869+OsamaBujwaied@users.noreply.github.com>
2021-04-04 18:30:50 +0300

    Update urls.py

--- prog1/urls.py
+++ prog1/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('assets/', include('assetstracking.urls')),
+    path('', include('assetstracking.urls')),
 ]
